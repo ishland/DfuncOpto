faa3ad80cb27fb8f3627421c3c910adde5e2fd6d
ishland <ishlandmc@yeah.net>
2023-07-13 18:24:45 +0800

    perf: implement fill for UnaryOperation

--- src/main/java/com/ishland/dfuncopto/mixin/dfts/MixinDFTUnaryOperation.java
+++ src/main/java/com/ishland/dfuncopto/mixin/dfts/MixinDFTUnaryOperation.java
@@ -5,6 +5,7 @@ import com.ishland.dfuncopto.common.DensityFunctionUtil;
 import com.ishland.dfuncopto.common.IDensityFunction;
 import com.ishland.dfuncopto.common.SharedConstants;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 import org.spongepowered.asm.mixin.Final;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DensityFunctionTypes.UnaryOperation.class)
-public class MixinDFTUnaryOperation implements IDensityFunction<DensityFunctionTypes.UnaryOperation>, DFCacheControl {
+public abstract class MixinDFTUnaryOperation implements IDensityFunction<DensityFunctionTypes.UnaryOperation>, DFCacheControl, DensityFunctionTypes.Unary {
 
     @Shadow @Final private DensityFunctionTypes.UnaryOperation.Type type;
 
@@ -86,5 +87,49 @@ public class MixinDFTUnaryOperation implements IDensityFunction<DensityFunctionT
             return (DensityFunctionTypes.UnaryOperation) visitor.apply((DensityFunction) this);
         }
         return (DensityFunctionTypes.UnaryOperation) visitor.apply(DensityFunctionTypes.UnaryOperation.create(this.type, apply));
+    }
+
+    @Override
+    public void fill(double[] densities, EachApplier applier) {
+        this.input.fill(densities, applier);
+        switch (this.type) {
+            case ABS -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    densities[i] = Math.abs(density);
+                }
+            }
+            case SQUARE -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    densities[i] = density * density;
+                }
+            }
+            case CUBE -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    densities[i] = density * density * density;
+                }
+            }
+            case HALF_NEGATIVE -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    densities[i] = density > 0.0 ? density : density * 0.5;
+                }
+            }
+            case QUARTER_NEGATIVE -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    densities[i] = density > 0.0 ? density : density * 0.25;
+                }
+            }
+            case SQUEEZE -> {
+                for (int i = 0, densitiesLength = densities.length; i < densitiesLength; i++) {
+                    final double density = densities[i];
+                    double d = MathHelper.clamp(density, -1.0, 1.0);
+                    densities[i] = d / 2.0 - d * d * d / 24.0;
+                }
+            }
+        }
     }
 }
